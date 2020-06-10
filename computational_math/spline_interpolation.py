976a9a4cdd4b118cbf061f937d1380f5755aa6c8
lenivoe <levishok@gmail.com>
2020-06-11 01:17:13 +0700

    spline small refactoring

--- computational_math/spline_interpolation.py
+++ computational_math/spline_interpolation.py
@@ -3,16 +3,16 @@ import numpy as np
 import computational_math.tridiagonal_matrix_algorithm as tma
 
 
-def get_spline_derivative(nodes_x, b, c, d):
+def get_spline_derivative(nodes, b, c, d):
     '''
         Возвращает интерполированную функцию по данным, расчитанным функцией calc_spline_data из таблицы значений функции.
-        - nodes_x - список узлов;
+        - nodes - список узлов;
         - a, b, c, d - коэффициенты сплана.
     '''
 
     def spline(x):
-        i = _bin_search(x, nodes_x)-1
-        dx = x - nodes_x[i+1]
+        i = _bin_search(x, nodes)-1
+        dx = x - nodes[i+1]
         
         # bi + ci*(x-xi) + di/2*(x-xi)^2
         return b[i]*dx + c[i]/2*dx**2 + d[i]/6*dx**3
@@ -20,16 +20,16 @@ def get_spline_derivative(nodes_x, b, c, d):
     return spline
 
 
-def get_spline(nodes_x, a, b, c, d):
+def get_spline(nodes, a, b, c, d):
     '''
         Возвращает интерполированную функцию по данным, расчитанным функцией calc_spline_data из таблицы значений функции.
-        - nodes_x - список узлов;
+        - nodes - список узлов;
         - a, b, c, d - коэффициенты сплана.
     '''
 
     def spline(x):
-        i = _bin_search(x, nodes_x)-1
-        dx = x - nodes_x[i+1]
+        i = _bin_search(x, nodes)-1
+        dx = x - nodes[i+1]
         
         # ai + bi*(x-xi) + ci/2*(x-xi)^2 + di/6*(x-xi)^3
         return a[i] + b[i]*dx + c[i]/2*dx**2 + d[i]/6*dx**3
@@ -37,19 +37,19 @@ def get_spline(nodes_x, a, b, c, d):
     return spline
 
 
-def calc_spline_data(nodes_x :np.ndarray, nodes_y :np.ndarray):
+def calc_spline_data(nodes :np.ndarray, values :np.ndarray):
     '''
         Возвращает вектора с коэффициентами a, b, c, d, необходимые для функции сплайна.
-        - nodes_x - список узлов;
-        - nodes_y - список значений интерполируемой функции в узлах.
+        - nodes - список узлов;
+        - values - список значений интерполируемой функции в узлах.
     '''
 
-    assert len(nodes_x) == len(nodes_y)
+    assert len(nodes) == len(values)
 
-    hx = nodes_x[1:] - nodes_x[:-1]
-    hy = nodes_y[1:] - nodes_y[:-1]
+    hx = nodes[1:] - nodes[:-1]
+    hy = values[1:] - values[:-1]
 
-    a = tuple(nodes_y[1:])
+    a = tuple(values[1:])
 
     c = np.array(tma.calc(_gen_tma_data(hx, hy)))
     c[0] = c[-1] = 0 # точные нули на всякий
