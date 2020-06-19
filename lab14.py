498d3d26400887c73716ae48d1700df730f0ff07
guseva-47 <guseva-47@yandex.ru>
2020-06-20 05:44:16 +0700

    p

--- lab14.py
+++ lab14.py
@@ -1,5 +1,6 @@
 from computational_math.runge_kutta import Runge_Kutta_with_auto_step
 import numpy as np
+
 # метод написан для решения системы из двух уравнений, причем первое уравнение (v) имеет начальное условие (решение в точке a, v(a) или v0),
 # а второе (u) не имеет, но имеет решение на правой границе интервала (u(b))
 # l_r -- интервал, внутри которого (как ожидается) лежит u(a)
@@ -9,7 +10,7 @@ def shooting_method(eq_system, a, b, va, ub, h, e) :
     x_vec, y_vec = find_ua(eq_system, a, b, va, ub, h, e, segment)
     return x_vec, y_vec
 
-def find_segment(eq_system, a, b, va, ub, h, e) :
+def find_segment(eq_system, a, b, l, r, va, ub, h, e) :
     
     def calc_ub(u0) :
         _, result = Runge_Kutta_with_auto_step(eq_system, np.array((va, u0)), a, b, e)
