8cff2459f53951a067c8ca84f49865eb9e3fa934
lenivoe <levishok@gmail.com>
2020-06-19 10:40:00 +0700

    now gauss can return None if failed

--- computational_math/gauss_method.py
+++ computational_math/gauss_method.py
@@ -1,29 +1,36 @@
 import numpy as np
 
-def gauss(a_mx :np.ndarray, b_vec :np.ndarray) -> np.ndarray:
+
+def gauss(a_mx: np.ndarray, b_vec: np.ndarray) -> np.ndarray:
     '''Метод Гаусса. a_mx - матрица из левой части СЛАУ. b_vec - столбец из правой части СЛАУ.'''
 
     a, b = np.copy(a_mx), np.copy(b_vec)
-    _forwardMove(a, b)
-    return _backwardMove(a, b)
+    if(_forwardMove(a, b)):
+        return _backwardMove(a, b)
+    return None
 
 
-def _forwardMove(a_mx :np.ndarray, b_vec :np.ndarray):
+def _forwardMove(a_mx: np.ndarray, b_vec: np.ndarray) -> bool:
     for i in range(0, np.size(a_mx, 0)):
         j = i + np.argmax(abs(a_mx[i:, i]))
+        if a_mx[i, j] == 0:
+            return False
+
         if i != j:
-            a_mx[(i, j),:] = a_mx[(j, i),:]
-            b_vec[(i, j),] = b_vec[(j, i),]
-        
+            a_mx[(i, j), :] = a_mx[(j, i), :]
+            b_vec[(i, j), ] = b_vec[(j, i), ]
+
         for j in range(i+1, np.size(a_mx, 0)):
             k = a_mx[j, i] / a_mx[i, i]
             a_mx[j, i:] -= k * a_mx[i, i:]
             b_vec[j] -= k * b_vec[i]
 
-def _backwardMove(a_mx :np.ndarray, b_vec :np.ndarray) -> np.ndarray:
+    return True
+
+
+def _backwardMove(a_mx: np.ndarray, b_vec: np.ndarray) -> np.ndarray:
     x = np.empty(b_vec.size)
     for i in range(b_vec.size-1, -1, -1):
         x[i] = (b_vec[i] - a_mx[i, i+1:].dot(x[i+1:])) / a_mx[i, i]
-    
-    return x
 
+    return x
