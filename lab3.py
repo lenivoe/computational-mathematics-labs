8588f21ed03e736e514b799f9fb340c828b33901
lenivoe <levishok@gmail.com>
2020-06-21 17:25:38 +0700

    lab3 fixed failing with bad values new tests

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from itertools import takewhile
+from itertools import takewhile, dropwhile
 import os
 import re
 
@@ -29,13 +29,16 @@ def main():
     with open(ifname, 'r', encoding='utf-8') as reader, open(ofname, 'w', encoding='utf-8') as writer:
         # print(gen_mx(3, 4, -9, 10), file=writer)
 
+        reader = reader.readlines()
+
         line_it = map(str.strip, reader)
         line_it = filter(lambda s: not s.startswith('#'), line_it)
         line_it = takewhile(lambda s: s != '--break', line_it)
 
         while True:
             try:
-                err = float(next(line_it))
+                it = dropwhile(lambda s: not s, line_it)
+                err = float(next(it))
                 x0_vec = to_numbers(next(line_it))
 
                 it = takewhile(lambda s: s, line_it)
@@ -47,19 +50,33 @@ def main():
                 a_mx = ab_mx[:, :-1]
                 b_vec = ab_mx[:, -1]
 
-                print('Диагональное преобладание:', less.diag_prevalence(a_mx), '\n', file=writer)
-                print('Достаточное число итераций:', *less.criteria(ab_mx, err, x0_vec), file=writer)
+                b_norm = less.mx_b_norm(a_mx)
+                print('Норма матрицы B:', b_norm, file=writer)
+                if b_norm < 1:
+                    jacobi_max_amount, seidel_max_amount = less.sufficient_condition(ab_mx, err, x0_vec)
+                    print('Достаточное число итераций метода Якоби:', jacobi_max_amount, file=writer)
+                    print('Достаточное число итераций метода Зейделя:', seidel_max_amount, file=writer)
+                else:
+                    print('Нет гарантии сходимости методов Якоби и Зейделя, так как ||B|| >= 1', file=writer)
+                print(file=writer)
 
                 x_vec, iters_amount = less.Jacobi_method(ab_mx, err, x0_vec)
-                print('Решение методом Якоби:', x_vec, file=writer)
-                print(f'Число итераций: {iters_amount}', file=writer)
-                print('Невязка', a_mx @ x_vec - b_vec, '\n', file=writer)
+                if iters_amount is np.nan:
+                    print('Не удалось вычислить решение методом Якоби', file=writer)
+                else:
+                    print('Решение методом Якоби:', x_vec, file=writer)
+                    print(f'Число итераций: {iters_amount}', file=writer)
+                    print('Невязка', a_mx @ x_vec - b_vec, file=writer)
+                print(file=writer)
 
                 x_vec, iters_amount = less.Seidel_method(ab_mx, err, x0_vec)
-                print('Решение методом Зейделя:', x_vec, file=writer)
-                print(f'Число итераций: {iters_amount}', file=writer)
-                print('Невязка', a_mx @ x_vec - b_vec, '\n', file=writer)
-                print(file=writer)
+                if iters_amount is np.nan:
+                    print('Не удалось вычислить решение методом Якоби', file=writer)
+                else:
+                    print('Решение методом Зейделя:', x_vec, file=writer)
+                    print(f'Число итераций: {iters_amount}', file=writer)
+                    print('Невязка', a_mx @ x_vec - b_vec, file=writer)
+                print('\n', file=writer)
 
             except StopIteration:
                 break
